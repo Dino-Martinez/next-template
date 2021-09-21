6d8fff4be6334023a48549d6fcae2087f3ee9dcc
Dino-Martinez <Dino.Martinez.514@gmail.com>
2021-09-21 15:08:42 -0400

    [Update] useApi
    stopped the fetch call from executing on component mount

--- hooks/useApi.js
+++ hooks/useApi.js
@@ -1,20 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-export default function useApi (url, options = {}, dependencies = []) {
+export default function useApi (url, options = {}, dependencies = [], runOnMount = false) {
   const [apiResult, setApiResult] = useState()
+  const [loading, setLoading] = useState(false)
+  const firstUpdate = useRef(!runOnMount)
 
   const refetch = () => {
+    setLoading(true)
     fetch(url, options)
       .then(res => res.json())
       .then(json => {
-        console.log(json)
         setApiResult(json)
+        setLoading(false)
       })
   }
 
   useEffect(() => {
-    refetch()
+    if (!firstUpdate.current) refetch()
+    if (firstUpdate.current) firstUpdate.current = false
   }, dependencies)
 
-  return [apiResult, refetch]
+  return [apiResult, refetch, loading]
 }
